361e8524abe8191f16b9e29f826a3adca83888b3
Zhien Li <li6834300@gmail.com>
2025-03-27 11:21:47 +0100

    working version

--- constants/Config.ts
+++ constants/Config.ts
@@ -5,7 +5,7 @@ export default {
   // API URL - default using localhost which works with Expo web
   API_URL: process.env.NODE_ENV === 'production' 
     ? 'https://desirabledifficult-backend.herokuapp.com' 
-    : 'https://145.127.69.26:3000',
+    : 'http://localhost:3000',
   
   // AI Provider endpoints
   AI_PROVIDERS: {
@@ -21,8 +21,8 @@ export default {
   
   // Network profiles for different environments
   NETWORK_PROFILES: {
-    OFFICE: 'https://145.127.69.26:3000',  // Office network IP
-    HOME: 'https://192.168.1.X:3000',      // Replace X with your home IP last digits
+    OFFICE: 'http://145.127.69.26:3000',  // Office network IP
+    HOME: 'http://192.168.1.X:3000',      // Replace X with your home IP last digits
     LOCALHOST: 'http://localhost:3000',
     PROD: 'https://desirabledifficult-backend.herokuapp.com'
   },
@@ -38,8 +38,6 @@ export default {
     AUTH_TOKEN: 'desirabledifficult_auth_token',
     USER_DATA: 'desirabledifficult_user_data',
     THEME: 'desirabledifficult_theme',
-    ACTIVE_NETWORK: 'desirabledifficult_active_network',
-    AI_PROVIDER: 'desirabledifficult_ai_provider',
-    DEEPSEEK_API_KEY: 'desirabledifficult_deepseek_key'
+    ACTIVE_NETWORK: 'desirabledifficult_active_network'
   }
 }; 
